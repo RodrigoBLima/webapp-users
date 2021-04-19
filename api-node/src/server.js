06ff3b1bcd033fc014006d5696253bd4f017c830
Rodrigo Barbosa <vanderdigo837@outlook.com>
2021-04-19 12:18:45 -0300

    add swagger

--- api-node/src/server.js
+++ api-node/src/server.js
@@ -5,6 +5,8 @@ const path = require("path")
 const env = require("dotenv")
 const db = require("./models");
 const initRoutes = require("./routes/");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
 global.__basedir = __dirname;
 
@@ -12,6 +14,7 @@ app.use(cors());
 app.options("*", cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 env.config()
 
